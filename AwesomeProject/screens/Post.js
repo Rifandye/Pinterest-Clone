f8ec44bd79b3d3eeddd3fe9d0a213cd2733636b8
Rifandye <rilfransyah@gmail.com>
2024-03-01 23:38:23 +0700

    feat: add a button to comment

--- AwesomeProject/screens/Post.js
+++ AwesomeProject/screens/Post.js
@@ -9,6 +9,7 @@ import {
   ScrollView,
 } from "react-native";
 import { useQuery, gql } from "@apollo/client";
+import Icon from "react-native-vector-icons/AntDesign";
 
 const GET_POSTS_BYID = gql`
   query PostById($postByIdId: ID!) {
@@ -56,15 +57,34 @@ export default function Post({ route }) {
 
   const { content, imgUrl, authorDetail, comments, likes } = data.postById;
 
+  const handleReviewPress = () => {
+    console.log("Review Button Pressed!");
+  };
+
   return (
     <ScrollView style={styles.container}>
       <View style={styles.imageContainer}>
         <Image source={{ uri: imgUrl }} style={styles.image}></Image>
       </View>
+      <View style={styles.likesContainer}>
+        <TouchableOpacity style={styles.heartButton}>
+          <Icon name="hearto" size={28} color="black" />
+        </TouchableOpacity>
+        {likes?.length > 0 && (
+          <>
+            <Text style={styles.likesText}>{likes.length} Likes</Text>
+            <Text style={styles.likeUsername}> by {likes[0].username}</Text>
+          </>
+        )}
+      </View>
       <View style={styles.information}>
-        <Text>Uplouded by: {authorDetail.username}</Text>
+        <Text>Uploaded by: {authorDetail.username}</Text>
+        <Text style={{ marginTop: 20 }}>Desciption:</Text>
         <Text>{content}</Text>
       </View>
+      <TouchableOpacity style={styles.reviewButton} onPress={handleReviewPress}>
+        <Text style={styles.reviewButtonText}>Give this photo a review</Text>
+      </TouchableOpacity>
     </ScrollView>
   );
 }
@@ -84,8 +104,33 @@ const styles = StyleSheet.create({
     resizeMode: "contain",
     borderRadius: 30,
   },
-  information: {
+  likesContainer: {
+    flexDirection: "row",
     alignItems: "center",
-    gap: 10,
+    padding: 10,
+    paddingLeft: 20, // Align content to the left with some padding
+  },
+  heartButton: {
+    marginRight: 15, // Add some space between the heart icon and the text
+  },
+  likesText: {},
+  likeUsername: {},
+  information: {
+    alignItems: "flex-start", // Align text to the left
+    paddingLeft: 20, // Align content to the left with some padding
+    gap: 15,
+  },
+  reviewButton: {
+    marginTop: 20,
+    backgroundColor: "#007bff", // Example blue color
+    padding: 15,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+    marginHorizontal: 20, // Add some margin to the sides
+  },
+  reviewButtonText: {
+    color: "#ffffff", // White text color
+    fontSize: 16,
   },
 });
